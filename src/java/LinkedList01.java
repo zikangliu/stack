d753c23880d8b090e57ec028197ee3960bb6c848
zikangliu <zikangliu0@gmail.com>
2023-07-28 21:18:23 +0800

    add LinkedList01

---
+++ src/java/LinkedList01.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+public class LinkedList01 {
+    public static void main(String[] args) {
+        LinkedList<String> arr = new LinkedList<String>();
+        arr.add("1");
+        arr.add("2");
+        arr.addLast("1997");
+        arr.add("3");
+        arr.add("4");
+        arr.add("5");
+
+//        for (int size = arr.size(), i = 0; i < size; i++) {
+//            System.out.print(arr.get(i) + " ");
+//        }
+
+        for (String i :
+                arr) {
+            System.out.print(i + " ");
+        }
+    }
+
+};
